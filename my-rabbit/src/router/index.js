193d6caa774368eb9ee26cec36f892f96ad039b1
shuhongfan <shfshuhongfan@gmail.com>
2023-06-05 15:39:43 +0800

    12. 支付页 1.基础数据渲染 2.支付功能实现 3.支付结果页展示 4.倒计时逻辑函数封装

--- my-rabbit/src/router/index.js
+++ my-rabbit/src/router/index.js
@@ -8,6 +8,7 @@ import Detail from "@/views/Detail/index.vue";
 import CartList from "@/views/CartList/index.vue";
 import Checkout from "@/views/Checkout/index.vue";
 import Pay from "@/views/Pay/index.vue";
+import PayBack from "@/views/PayBack/index.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -38,6 +39,9 @@ const router = createRouter({
                 }, {
                     path: 'pay',
                     component: Pay
+                }, {
+                    path: 'paycallback',
+                    component: PayBack
                 }
             ]
         }, {
--- my-rabbit/src/views/Pay/index.vue
+++ my-rabbit/src/views/Pay/index.vue
@@ -2,12 +2,25 @@
 import {onMounted, ref} from "vue";
 import {getOrderAPI} from "@/apis/pay";
 import {useRoute} from "vue-router";
+import {useCountDown} from "@/composables/useCountDown";
+
+const {formatTime, start}=useCountDown()
 
 const payInfo = ref({})
 const route=useRoute()
+// 支付地址
+const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net/'
+const backURL = 'http://127.0.0.1:5173/paycallback'
+const redirectUrl = encodeURIComponent(backURL)
+const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redirectUrl}`
+
+// 获取订单数据
 const getOrder=async ()=>{
   const res = await getOrderAPI(route.query.id);
   payInfo.value=res.result
+
+  // 初始化倒计时秒数
+  start(res.result.countdown)
 }
 onMounted(() => getOrder());
 </script>
@@ -21,7 +34,7 @@ onMounted(() => getOrder());
         <span class="icon iconfont icon-queren2"></span>
         <div class="tip">
           <p>订单提交成功！请尽快完成支付。</p>
-          <p>支付还剩 <span>24分30秒</span>, 超时后将取消订单</p>
+          <p>支付还剩 <span>{{ formatTime }}</span>, 超时后将取消订单</p>
         </div>
         <div class="amount">
           <span>应付总额：</span>
